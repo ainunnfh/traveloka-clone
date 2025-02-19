3f3f1560339dc714ce42d0243aea15e0b2e5ca54
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-19 17:25:17 +0700

    💄hotel form: change rounded size

--- src/components/HotelForm.tsx
+++ src/components/HotelForm.tsx
@@ -3,51 +3,46 @@ import { Calendar, MapPin, Search, Users } from "lucide-react";
 
 const HotelForm = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-8 gap-2">
-      <div className="flex w-3/4 max-w-4xl justify-between px-4 text-white">
-        <div className="flex flex-col items-start">
-          <label className="relative text-sm font-semibold">
+    <div className="flex flex-col items-center justify-start py-8 gap-4">
+      <div className="relative flex items-center w-full max-w-4xl bg-white rounded-lg shadow-lg px-4 py-1 gap-4">
+        <div className="flex flex-col flex-1">
+          <label className="text-xs text-gray-500 mb-1">
             City, destination, or hotel name
           </label>
+          <div className="flex items-center gap-2">
+            <MapPin className="text-blue-600" />
+            <input
+              type="text"
+              placeholder="City, hotel, place to go"
+              className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
+            />
+          </div>
         </div>
-        <div className=" relative flex flex-col items-start">
-          <label className="text-sm font-semibold">
+
+        <div className="flex flex-col flex-1 border-l border-gray-300 pl-4">
+          <label className="text-xs text-gray-500 mb-1">
             Check-in & Check-out Dates
           </label>
-        </div>
-        <div className="flex flex-col items-start">
-          <label className="relative text-sm font-semibold">
-            Guests and Rooms
-          </label>
-        </div>
-      </div>
-
-      <div className="relative flex items-center w-3/4 max-w-4xl bg-white rounded-full shadow-lg px-4 py-2 gap-4">
-        <div className="flex flex-1 items-center gap-2">
-          <MapPin className="text-blue-600" />
-          <input
-            type="text"
-            placeholder="City, hotel, place to go"
-            className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
-          />
+          <div className="flex items-center gap-2">
+            <Calendar className="text-blue-600" />
+            <input
+              type="text"
+              placeholder="20 Feb 2025 - 21 Feb 2025"
+              className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-1 items-center gap-2 border-l border-gray-300 pl-4">
-          <Calendar className="text-blue-600" />
-          <input
-            type="text"
-            placeholder="20 Feb 2025 - 21 Feb 2025"
-            className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
-          />
-        </div>
-
-        <div className="flex flex-1 items-center gap-2 border-l border-gray-300 pl-4">
-          <Users className="text-blue-600" />
-          <input
-            type="text"
-            placeholder="1 Adult(s), 0 Child, 1 Room"
-            className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
-          />
+        <div className="flex flex-col flex-1 border-l border-gray-300 pl-4">
+          <label className="text-xs text-gray-500 mb-1">Guests and Rooms</label>
+          <div className="flex items-center gap-2">
+            <Users className="text-blue-600" />
+            <input
+              type="text"
+              placeholder="1 Adult(s), 0 Child, 1 Room"
+              className="w-full outline-none bg-transparent placeholder-gray-500 text-gray-900"
+            />
+          </div>
         </div>
 
         <button className="bg-orange-500 text-white p-4 rounded-full">
