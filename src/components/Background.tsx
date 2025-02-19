118ac04948cce155592d8c1acb1ccf34f6d40a6c
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-19 13:25:17 +0700

    ⭐create bg comp

---
+++ src/components/Background.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-[75vh]">
+      <img
+        src="bromo-mountain3.jpg"
+        alt="Bromo Mountain"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+};
+
+export default Background;
