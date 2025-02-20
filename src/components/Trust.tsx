fef85ee2be07ef8db37f2d491a799a89687cc9b9
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-20 11:12:08 +0700

    ⭐create trust comp

---
+++ src/components/Trust.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Trust = () => {
+  return (
+    <div className="relative p2 text-white flex justify-center gap-2">
+      Trusted by
+      <div>Artotel Group</div>
+      <div>Dprimahotel</div>
+      <div>Santika hotel</div>
+      <div>Jambuluk</div>
+    </div>
+  );
+};
+
+export default Trust;
