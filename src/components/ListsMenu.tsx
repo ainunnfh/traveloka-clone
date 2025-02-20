348b8fc5dcb6e14a13b0f38c4b43becf4e1d505e
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-20 11:11:46 +0700

    🆙list menu, main navbar, navbar: add scroll effect

--- src/components/ListsMenu.tsx
+++ src/components/ListsMenu.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Percent } from "lucide-react";
-const ListsMenu = () => {
+
+interface IListsMenuProps {
+  isScrolled: boolean;
+}
+const ListsMenu: React.FC<IListsMenuProps> = (props) => {
   return (
-    <div className="flex flex-col md:flex-row md:items-center text-white text-sm md:text-md font-semibold">
+    <div
+      className={`flex flex-col md:flex-row md:items-center text-sm md:text-md font-semibold transition-all duration-300 ${
+        props.isScrolled ? "text-black" : "text-white"
+      }`}
+    >
       <div className="flex gap-1 items-center hover:bg-black/20 hover:cursor-pointer py-2 md:px-2 rounded-md">
         <img src="icons-indonesia.png" alt="" className="w-6" />
-        <span>EN | IDR</span>{" "}
+        <span>EN | IDR</span>
       </div>
       <div className="flex items-center gap-1 hover:bg-black/20 hover:cursor-pointer py-2 md:px-2 rounded-md">
-        <Percent size={12} color="green" />
-        <span> Promo</span>
+        <Percent size={12} color={props.isScrolled ? "green" : "white"} />
+        <span>Promo</span>
       </div>
       <div className="hover:bg-black/20 hover:cursor-pointer py-2 md:px-2 rounded-md">
         Deals
@@ -27,11 +35,23 @@ const ListsMenu = () => {
         Bookings
       </div>
 
-      <div className="hover:bg-black/20 hover:cursor-pointer p-1 rounded-md flex gap-2 border border-white">
-        <img src="icon-user-white.png" alt="" className="w-5" />
+      <div
+        className={`hover:bg-black/20 hover:cursor-pointer p-1 rounded-md flex gap-2 border ${
+          props.isScrolled ? "border-[#0094F3]" : "border-white"
+        }`}
+      >
+        <img
+          src={props.isScrolled ? "icon-user-blue.png" : "icon-user-white.png"}
+          alt=""
+          className="w-5"
+        />
         <span>Log In</span>
       </div>
-      <div className="hover:bg-blue-600 ml-3 font-bold hover:cursor-pointer py-1.5 md:px-1 rounded-md flex justify-around bg-[#0094F3]">
+      <div
+        className={`hover:bg-blue-600 ml-3 font-bold hover:cursor-pointer py-1.5 md:px-1 rounded-md flex justify-around bg-[#0094F3] ${
+          props.isScrolled ? "text-white" : ""
+        }`}
+      >
         Register
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 
-const Navbar = () => {
+interface INavbarProps {
+  isScrolled: boolean;
+}
+const Navbar: React.FC<INavbarProps> = (props) => {
   return (
-    <div className="relative p-2 flex justify-between md:justify-start md:gap-6 text-white font-bold text-xs md:text-base border-b border-slate-200 ">
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Hotels
-      </div>
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Flights
-      </div>
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Trains
-      </div>
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Bus & Travel
-      </div>
-      {/* <div>Airport Transfer</div> */}
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Car Rental
-      </div>
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        Things to Do
-      </div>
-      <div className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md">
-        More
-      </div>
+    <div
+      className={`relative p-2 flex justify-between md:justify-start md:gap-6 font-bold text-xs md:text-base border-b border-slate-200 transition-all duration-300 ${
+        props.isScrolled
+          ? "bg-white text-black shadow-md"
+          : "bg-transparent text-white"
+      }`}
+    >
+      {[
+        "Hotels",
+        "Flights",
+        "Trains",
+        "Bus & Travel",
+        "Car Rental",
+        "Things to Do",
+        "More",
+      ].map((item, index) => (
+        <div
+          key={index}
+          className="hover:cursor-pointer hover:bg-black/20 py-1 px-2 rounded-md "
+        >
+          {item}
+        </div>
+      ))}
     </div>
   );
 };
