c406f2c21db5991f81a9904966cb72e59298fe7b
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-20 10:08:52 +0700

    🆙page: usage some comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import Background from "@/components/Background";
+import Coupons from "@/components/Coupons";
+import Features from "@/components/Features";
+import HotelForm from "@/components/HotelForm";
 import MainNavbar from "@/components/MainNavbar";
+import Navbar from "@/components/Navbar";
+import SubFeatures from "@/components/SubFeatures";
+import Trust from "@/components/Trust";
 
 export default function Home() {
   return (
-    <>
+    <div className="p-6">
       <Background />
-      <div className="p-4">
-        <MainNavbar />
+      <MainNavbar />
+      <Navbar />
+      <div className="relative text-3xl text-center text-white font-bold mt-8">
+        From Southeast Asia to the World, All Yours.
       </div>
-    </>
+
+      <div className="mt-4">
+        <Features />
+        <SubFeatures />
+        <HotelForm />
+        <Trust />
+      </div>
+      <div className="relative w-full md:bottom-[-13vh]">
+        <Coupons />
+      </div>
+    </div>
   );
 }
